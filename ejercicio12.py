820f14481e938f446dcff3342be6c8f8af7ac32a
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:25:54 -0500

    ejercicio12.py

---
+++ ejercicio12.py
@@ -0,0 +1,18 @@
+#Calcular K
+x,y,z,w,K=0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+x=15
+y=3
+z=4
+w=3
+
+#Calculo
+K=((x/w)+(y**w))-z
+
+#Mostrar valores
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+print("w = ", w)
+print("K = ", K)
