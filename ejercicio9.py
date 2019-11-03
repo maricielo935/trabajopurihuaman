147a2e2692a53e62c743c61eff8fcb0bf3c22c68
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:23:28 -0500

    ejercicio9.py

---
+++ ejercicio9.py
@@ -0,0 +1,16 @@
+#Calcular M
+x,y,z,M=0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+x=42
+y=6
+z=10
+
+#Calculo
+M=(x-y)*z
+
+#Mostrar valores
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+print("M = ", M)
