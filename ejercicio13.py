11dcf4573ccfc9dce91dfa1ccfd8feae0b55b219
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:26:29 -0500

    ejercicio13.py

---
+++ ejercicio13.py
@@ -0,0 +1,18 @@
+#Calcular Z
+a,b,c,d,Z=0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+a=126
+b=3
+c=7
+d=9
+
+#Calculo
+Z=(a/c-(b*b))+d
+
+#Mostrar valores
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+print("d = ", d)
+print("Z = ", Z)
