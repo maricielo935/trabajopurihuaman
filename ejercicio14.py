f53601b155f8376ce576ca582744d4a81bd74ade
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:28:14 -0500

    ejercicio14.py

---
+++ ejercicio14.py
@@ -0,0 +1,16 @@
+#Calcular el perimetro del triangulo
+lado1,lado2,lado3,perimetro=0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+lado1=18
+lado2=5
+lado3=13
+
+#Calculo
+perimetro=lado1+lado2+lado3
+
+#Mostrar valores
+print("lado1 = ", lado1)
+print("lado2 = ", lado2)
+print("lado3 = ", lado3)
+print("perimetro = ", perimetro)
