0e8086743bc45286470482cda80ca0acb2bdf6b3
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:25:09 -0500

    ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,14 @@
+#Calcular el area del cuadrado
+lado1,lado2,area=0.0,0.0,0.0
+
+#Asignacion de valores
+lado1=12
+lado2=12
+
+#Calculo
+area=lado1*lado2
+
+#Mostrar valores
+print("lado1 = ", lado1)
+print("lado2 = ", lado2)
+print("area = ", area)
