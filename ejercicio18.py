21e00aa0263904885602e12ed43d99aa08fefe3f
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:36:07 -0500

    ejercicio18.py

---
+++ ejercicio18.py
@@ -0,0 +1,14 @@
+#Calcular el area del circulo
+π,radio=0.0,0.0
+
+#Asignacion de valores
+π=3.14
+radio=25
+
+#Calculo
+area= (π)*(radio**2)
+
+#Mostrar valores
+print("π = ", π)
+print("radio = ", radio)
+print("area = ", area)
