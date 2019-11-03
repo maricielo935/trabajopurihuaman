6a3b7f9a36f9d7aa774a2d7003dc269376b5a550
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:37:27 -0500

    ejercicio19.py

---
+++ ejercicio19.py
@@ -0,0 +1,14 @@
+#Calcular cuanto dinero le corresponde a cada nieto
+numero_de_nietos,dinero_total,dinero_para_cada_uno=0.0,0.0,0.0
+
+#Asignacion de valores
+numero_de_nietos=5
+dinero_total= 2500
+
+#Calculo
+dinero_para_cada_uno= dinero_total/numero_de_nietos
+
+#Mostrar valores
+print("numero_de_nieto = ", numero_de_nietos)
+print("dinero_total = ", dinero_total)
+print("dinero_para_cada_uno = ", dinero_para_cada_uno)
