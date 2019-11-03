a7be5116bd2803bab7980a7970e5b47bb7de92a4
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:22:44 -0500

    ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,14 @@
+#Calcular el area del rombo
+diagonal1,diagonal2,area=0.0,0.0,0.0
+
+#Asignacion de valores
+diagonal1=30
+diagonal2=35
+
+#Calculo
+area=(diagonal1*diagonal2)/2
+
+#Mostrar valores
+print("diagonal1 = ", diagonal1)
+print("diagonal2 = ", diagonal2)
+print("area = ", area)
