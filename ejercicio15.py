074d5de5673349c6d5a578cf7b324397682dc69e
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:28:57 -0500

    ejercicio15.py

---
+++ ejercicio15.py
@@ -0,0 +1,14 @@
+#Calcular el tercer angulo
+angulo1,angulo2,angulo3=0.0,0.0,0.0
+
+#Asignacion de valores
+angulo1=90
+angulo2=30
+
+#Calculo
+angulo3=angulo1-angulo2
+
+#Mostrar valores
+print("angulo1 = ", angulo1)
+print("angulo2 = ", angulo2)
+print("angulo3 = ", angulo3)
