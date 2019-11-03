376f101c46f591b77b450ca9f922a1c2e5b8410a
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:29:42 -0500

    ejercicio16.py

---
+++ ejercicio16.py
@@ -0,0 +1,14 @@
+#Calcular el tercer angulo
+minuendo,sustraendo,resultado=0.0,0.0,0.0
+
+#Asignacion de valores
+minuendo=624
+sustraendo=169
+
+#Calculo
+resultado=minuendo-sustraendo
+
+#Mostrar valores
+print("minuendo = ", minuendo)
+print("sustraendo = ", sustraendo)
+print("resultado = ", resultado)
