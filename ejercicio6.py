e3c202730a2ed1e4b2e5100e07d51b49cb5ca561
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:20:27 -0500

    ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,16 @@
+#Teorema de Poncelet
+cateto1,cateto2,hipotenusa,radio=0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+cateto1=30
+cateto2=40
+hipotenusa=50
+
+#Calculo
+radio=(cateto1+cateto2-hipotenusa)/2
+
+#Mostrar valores
+print("cateto1 = ", cateto1)
+print("cateto2 = ", cateto2)
+print("hipotenusa =", hipotenusa)
+print("radio =", radio)
