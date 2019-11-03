5dd83a1dc9ddb6867bd9af132a290063f96f26a6
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:19:30 -0500

    ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,14 @@
+#Calcular el area de la superficie esferica
+π,radio=0.0,0.0
+
+#Asignacion de valores
+π=3.14
+radio=5
+
+#Calculo
+area= (4*π)*(radio**2)
+
+#Mostrar valores
+print("π = ", π)
+print("radio = ", radio)
+print("area = ", area)
