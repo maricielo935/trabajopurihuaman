c007bbc4c3543dfd2c52a8079025d3a5e711c578
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:16:32 -0500

    ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,16 @@
+#Calcular el angulo w del cuadrilatero
+x,y,z,w=0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+x=140
+y=78
+z=55
+
+#Calculo
+w= 360-(x+y+z)
+
+#Mostrar valores
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+print("w = ", w)
