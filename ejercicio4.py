1d2ad548a6a9506bc9729ce6d9f947db61ceee70
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:18:43 -0500

    ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,18 @@
+#Calcular el promedio aritmetico de las siguientes alumnos
+alumno1,alumno2,alumno3,alumno4,promedio=0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+alumno1=10
+alumno2=19
+alumno3=7
+alumno4=13
+
+#Calculo
+promedio=(alumno1+alumno2+alumno3+alumno4)/4
+
+#Mostrar valores
+print("alumno1 = ", alumno1)
+print("alumno2 = ", alumno2)
+print("alumno3 = ", alumno3)
+print("alumno4 = ", alumno4)
+print("promedio = ", promedio)
