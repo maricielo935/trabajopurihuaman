545bc3f33f50240de6f50893c6cf0f19f858dee8
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:38:07 -0500

    ejercicio20.py

---
+++ ejercicio20.py
@@ -0,0 +1,14 @@
+#Calcular el area del terreno rectangular
+largo,ancho,area=0.0,0.0,0.0
+
+#Asignacion de valores
+largo=80
+ancho=50
+
+#Calculo
+area=(largo*ancho)
+
+#Mostrar valores
+print("largo = ", largo)
+print("ancho = ", ancho)
+print("area = ", area)
