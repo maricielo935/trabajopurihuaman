6df69d2adbeecace6126505df9a9188863a3f599
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:35:25 -0500

    ejercicio17.py

---
+++ ejercicio17.py
@@ -0,0 +1,17 @@
+#Calcular el dinero recaudado
+Maricielo,Jhossely,Ariana,Sarai,dinero_recaudado=0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+Maricielo=58.25
+Jhossely=30.00
+Ariana=18.75
+Sarai=6.00
+#Calculo
+dinero_recaudado=Maricielo+Jhossely+Ariana+Sarai
+
+#Mostrar valores
+print("Maricielo = ", Maricielo)
+print("Jhossely = ", Jhossely)
+print("Ariana = ", Ariana)
+print("Sarai = ", Sarai)
+print("dinero_recaudado = ", dinero_recaudado)
