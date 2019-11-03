fa1c6bf9e03847411aa527c3f8948b27f72e7a42
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:24:10 -0500

    ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,20 @@
+#Calcular N
+m,l,n,o,p,N=0.0,0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+m=8
+l=4
+n=2
+o=6
+p=9
+
+#Calculo
+N=(m+(l**n))/o+p
+
+#Mostrar valores
+print("m = ",m)
+print("l = ",l)
+print("n = ",n)
+print("o = ",o)
+print("p = ",p)
+print("N = ",N)
