960e71d611bcc1f16f6eef9fd480db31e6ad9557
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-03 14:21:46 -0500

    ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,18 @@
+#Calcular X
+a,b,c,d,X=0.0,0.0,0.0,0.0,0.0
+
+#Asignacion de valores
+a=90
+b=45
+c=5
+d=2
+
+#Calculo
+X=((a-b)/c)**d
+
+#Mostrar valores
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+print("d = ", d)
+print("X = ", X)
